65e3b8fa2212b9df74e302ed2801f94a33373eb2
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2021-09-20 14:44:48 +0300

    Initial Commit

---
+++ TryPushNotifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryPushNotifications
+//
+//  Created by Ilya Cherkasov on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
